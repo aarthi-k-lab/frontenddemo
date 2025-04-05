449bb655f1408ad9695f5289c9da17e8bc3b1110
aarthi-k-lab <aarthikannan2103@gmail.com>
2025-04-06 03:04:31 +0530

    changing the backend server

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000'; // or your backend URL
+const BASE_URL = 'https://bookmyshowbackend-fg92.onrender.com'; // or your backend URL
 
 export const getMoviesByCity = async (cityName) => {
   const response = await axios.get(`${BASE_URL}/api/cities/${cityName}/movies`);
